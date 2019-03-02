0c2f3ed0c4e79ab7cf795faf8d52475cbd1cdd60
Aoife McManus <amcmanu4@binghamton.edu>
2019-03-02 17:07:00 -0500

    made a js file from search.py

--- search.py
+++ search.py
@@ -1,6 +1,5 @@
 import json
 import requests
-from bs4 import BeautifulSoup
 def main():
     search = input("What item are you looking for? ")
     searchResult = requests.get("https://api.wegmans.io/products/search?query=" + search + "&api-version=2018-10-18&subscription-key=f8d017e8fbe7429a9bd8e1ba49131ce1")
