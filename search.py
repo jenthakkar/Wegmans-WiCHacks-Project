88a43ceaa959fc124bfa9f2b7bcc301db6dcc335
Aoife McManus <amcmanu4@binghamton.edu>
2019-03-02 16:20:03 -0500

    takes input for search

---
+++ search.py
@@ -0,0 +1,10 @@
+import json
+import requests
+from bs4 import BeautifulSoup
+def main():
+    search = input("What item are you looking for? ")
+    searchResult = requests.get("https://api.wegmans.io/products/search?query=" + search + "&api-version=2018-10-18&subscription-key=f8d017e8fbe7429a9bd8e1ba49131ce1")
+    searchResult.text
+    searchResult.json()
+    print(searchResult.text)
+main()
